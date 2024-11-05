61d850e6702e5c248c83e205bb1806f47f2bcc38
Helena Oliveira <helena.oliveira@viking.ind.br>
2024-11-05 10:45:44 -0300

    Ajustando configuração para Railway

---
+++ run.py
@@ -0,0 +1,4 @@
+from app import app  # ou o caminho correto para sua instância Flask
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000)  # Certifique-se de que a porta está correta
