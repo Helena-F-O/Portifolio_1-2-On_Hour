8bcdcd9d4b22550e36e9e282c16696954a38ae6c
Helena Felicia <helenafeliciaoliveira@gmail.com>
2024-11-11 20:41:17 -0300

    Ajustando Railway

--- app.py
+++ app.py
@@ -315,5 +315,5 @@ def download_certificados():
     return send_file(pdf_buffer, as_attachment=True, download_name='certificados.pdf', mimetype='application/pdf')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
